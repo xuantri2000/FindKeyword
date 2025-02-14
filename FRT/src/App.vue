e4a9afbb550a7230da79739c657d40e3e20be454
Xuan Tri <xuantri2000@gmail.com>
2025-02-14 10:17:29 +0700

    A

--- FRT/src/App.vue
+++ FRT/src/App.vue
@@ -1,98 +1,32 @@
 <script setup>
-import { ref, onMounted } from "vue";
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from 'vue-router';
+import feather from 'feather-icons';
+import { onMounted } from 'vue';
 
-const message = ref("");
-
-onMounted(async () => {
-    try {
-        const response = await fetch("api/records/select");
-        const data = await response.json();
-        message.value = data.message;
-    } catch (error) {
-        console.error("Error fetching data:", error);
-    }
+onMounted(() => {
+  feather.replace();
 });
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+	<nav class="navbar">
+		<ul class="navbar__menu">
+			<li class="navbar__item">
+				<RouterLink href="#" class="navbar__link"><i data-feather="home"></i><span>Home</span></RouterLink>
+			</li>
+			<li class="navbar__item">
+				<RouterLink	RouterLink href="#" class="navbar__link"><i data-feather="message-square"></i><span>Messages</span></RouterLink>        
+			</li>
+			<li class="navbar__item">
+				<RouterLink href="#" class="navbar__link"><i data-feather="users"></i><span>Customers</span></RouterLink>        
+			</li>
+			<li class="navbar__item">
+				<RouterLink href="#" class="navbar__link"><i data-feather="folder"></i><span>Projects</span></RouterLink>        
+			</li>
+		</ul>
+	</nav>
   </header>
 
   <RouterView />
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
